245932edd115c01b7fbec72982df963189ba3ffa
Dave Champion <183235038+davechampion-dev@users.noreply.github.com>
2025-03-17 21:34:17 +0000

    Latest update

--- src/DavesDartsClub.Aspire.AppHost/Program.cs
+++ src/DavesDartsClub.Aspire.AppHost/Program.cs
@@ -4,4 +4,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var api = builder.AddProject<Projects.DavesDartsClub_WebApi>("WebApi");
 
+var website = builder.AddProject<Projects.DavesDartsClub_Website>("Website")
+    .WithReference(api)
+    .WaitFor(api);
+
 builder.Build().Run();
