8b901edbdebbeed685e6b632c0a61fdb00ea5e02
Dave Champion <183235038+davechampion-dev@users.noreply.github.com>
2025-05-13 21:57:50 +0100

    League & Tournament added to Domain

---
+++ src/DavesDartsClub.Domain/Tournament.cs
@@ -0,0 +1,8 @@
+﻿namespace DavesDartsClub.Domain;
+
+public class Tournament
+{
+    public const int TournamentNameMaxLength = 50;
+    public Guid TournamentId { get; init; }
+    public string TournamentName { get; set; }
+}
