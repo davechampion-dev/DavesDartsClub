a9b7528b230b8194b05064e311a6ac1cc2f0ef5e
Dave Champion <183235038+davechampion-dev@users.noreply.github.com>
2025-05-13 21:56:41 +0100

    Added Tournament Service

---
+++ src/DavesDartsClub.Application/ITournamentService.cs
@@ -0,0 +1,10 @@
+﻿
+using DavesDartsClub.Domain;
+
+namespace DavesDartsClub.Application;
+
+public interface ITournamentService
+{
+    Tournament GetTournamentById(Guid tournamentId);
+    Tournament GetTournamentByName(string name);
+}
---
+++ src/DavesDartsClub.Application/TournamentService.cs
@@ -0,0 +1,25 @@
+﻿using DavesDartsClub.Domain;
+
+namespace DavesDartsClub.Application;
+
+public class TournamentService : ITournamentService
+{
+    
+    public Tournament GetTournamentById(Guid tournamentId)
+    {
+        return new Tournament()
+        {
+            TournamentId = tournamentId,
+            TournamentName = "Champions League"
+        };
+    }
+
+    public Tournament GetTournamentByName(string name)
+    {
+        return new Tournament()
+        {
+            TournamentId = Guid.NewGuid(),
+            TournamentName = "Champions League"
+        };
+    }
+}
