22f6503854302d3b8229b4181bbd0a9bc7cf4941
Dave Champion <183235038+davechampion-dev@users.noreply.github.com>
2025-09-28 21:21:01 +0100

    Added editor config, xml doc comments , Centralised build props

--- src/DavesDartsClub.Infrastructure/Extensions.cs
+++ src/DavesDartsClub.Infrastructure/Extensions.cs
@@ -12,14 +12,29 @@ public static class Extensions
         
         return services;
     }
-
-    // todo: constance to hold the connection string name
+    
+    /// <summary>
+    /// Configures the application to use the <see cref="AppDbContext"/> with a SQL Server database.
+    /// </summary>
+    /// <remarks>This method registers the <see cref="AppDbContext"/> with the dependency injection container,
+    /// using a connection string named "DavesDartsClubDatabase". Ensure that the connection string  is properly
+    /// configured in the application's configuration file.</remarks>
+    /// <param name="builder">The <see cref="IHostApplicationBuilder"/> used to configure the application's services.</param>
     public static void AddDavesDarstClubAppDbContext(this IHostApplicationBuilder builder)
         => builder.AddSqlServerDbContext<AppDbContext>("DavesDartsClubDatabase");
+    // todo: constance to hold the connection string name
 
-    // this is a workaround for how aspire doesnt use seeding with core migration
-    // see link https://juliocasal.com/blog/how-to-seed-data-with-ef-core-9-and-net-aspire
-
+    /// <summary>
+    /// Configures the application to use the <see cref="AppDbContext"/> with a SQL Server database  and applies data
+    /// seeding during migrations.
+    /// </summary>
+    /// <remarks>
+    /// This is a workaround for how aspire doesnt use seeding with core migration.
+    /// <seealso cref="https://juliocasal.com/blog/how-to-seed-data-with-ef-core-9-and-net-aspire"/>
+    /// This method sets up the <see cref="AppDbContext"/> to use a SQL Server database with the 
+    /// connection string named "DavesDartsClubDatabase". It also ensures that data seeding is  performed during
+    /// migrations, both synchronously and asynchronously, if the database is empty.</remarks>
+    /// <param name="builder">The <see cref="IHostApplicationBuilder"/> used to configure the application's services.</param>
     public static void AddDavesDarstClubAppDbContextForMigration(this IHostApplicationBuilder builder)
         => builder.AddSqlServerDbContext<AppDbContext>(
             "DavesDartsClubDatabase",
