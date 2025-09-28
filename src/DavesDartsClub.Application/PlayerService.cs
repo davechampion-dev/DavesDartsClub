e6c24ad488a86edf5fc59e6bbad94ffbc92bc4b9
Dave Champion <183235038+davechampion-dev@users.noreply.github.com>
2025-09-28 19:25:08 +0100

    Added validation to player service

--- src/DavesDartsClub.Application/PlayerService.cs
+++ src/DavesDartsClub.Application/PlayerService.cs
@@ -1,9 +1,16 @@
 ﻿using DavesDartsClub.Domain;
+using FluentValidation;
 
 namespace DavesDartsClub.Application;
 
 public class PlayerService : IPlayerService
 {
+    private readonly IValidator<Player> _playerValidator;
+
+    public PlayerService(IValidator<Player> playerValidator)
+    {
+        _playerValidator = playerValidator;
+    }
 
     public Player GetPlayerById(Guid playerId)
     {
