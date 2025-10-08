bec11c6a7d8e5c4622ff19e80d035c177430bb9d
Dave Champion <183235038+davechampion-dev@users.noreply.github.com>
2025-10-08 11:36:08 +0100

    Added MemberValidatorUnitTest

---
+++ src/DavesDartsClub.UnitTests/Domain/Validation/MemberValidatorUnitTest.cs
@@ -0,0 +1,38 @@
+﻿using DavesDartsClub.Application;
+using DavesDartsClub.Domain;
+using DavesDartsClub.Domain.Validation;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DavesDartsClub.UnitTests.Domain.Validation;
+
+public class MemberValidatorUnitTest
+{
+    private readonly MemberValidator _memberValidator;
+    public MemberValidatorUnitTest()
+    {
+        _memberValidator = new MemberValidator();
+    }
+
+    [Fact]
+    public void Validate_Should_ReturnAValidResponseWithNoErrors_Given_AValidMember()
+    {
+        //Arrange
+        var validMember = new Member
+        {
+            MemberId = Guid.NewGuid(),
+            MemberName = "Edd the Duck"
+        };
+
+        //Act
+        var response = _memberValidator.Validate(validMember);
+
+        //Assert
+        response.IsValid.ShouldBeTrue();
+        response.Errors.ShouldBeEmpty();
+    }
+}
