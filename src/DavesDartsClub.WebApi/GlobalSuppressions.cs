e4059b46e7474fb140a406382a32cbf07f4c179e
Dave Champion <183235038+davechampion-dev@users.noreply.github.com>
2026-02-23 23:34:17 +0000

    updated code analysis suppressions

--- src/DavesDartsClub.WebApi/GlobalSuppressions.cs
+++ src/DavesDartsClub.WebApi/GlobalSuppressions.cs
@@ -3,8 +3,6 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-using System.Diagnostics.CodeAnalysis;
-
 [assembly: SuppressMessage("Critical Code Smell", "S4487:Unread \"private\" fields should be removed", Justification = "<Pending>", Scope = "member", Target = "~F:DavesDartsClub.WebApi.Controllers.ScoreController._logger")]
 [assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Controller Code", Scope = "member", Target = "~M:DavesDartsClub.WebApi.Controllers.LeagueController.CreateLeague(DavesDartsClub.SharedContracts.League.LeagueRequest,System.Threading.CancellationToken)~System.Threading.Tasks.Task{Microsoft.AspNetCore.Mvc.ActionResult{System.Guid}}")]
 [assembly: SuppressMessage("Design", "CA1062:Validate arguments of public methods", Justification = "Controller Code", Scope = "member", Target = "~M:DavesDartsClub.WebApi.Controllers.MemberController.CreateMember(DavesDartsClub.SharedContracts.Member.MemberRequest,System.Threading.CancellationToken)~System.Threading.Tasks.Task{Microsoft.AspNetCore.Mvc.ActionResult{System.Guid}}")]
