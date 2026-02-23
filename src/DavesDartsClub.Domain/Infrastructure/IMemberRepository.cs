71d57cdecd81ccf3ec7c71ae9c0b0025f0f39f0d
Dave Champion <183235038+davechampion-dev@users.noreply.github.com>
2026-02-23 23:15:28 +0000

    Member service refactor

--- src/DavesDartsClub.Domain/Infrastructure/IMemberRepository.cs
+++ src/DavesDartsClub.Domain/Infrastructure/IMemberRepository.cs
@@ -4,5 +4,5 @@ public interface IMemberRepository
 {
     Task<Member> AddMember(Member member, CancellationToken cancellationToken);
     Task<Member?> GetMemberByIdAsync(Guid memberId, CancellationToken cancellationToken);
-    
+    Task<List<Member>> GetMemberByNameAsync(string memberName, CancellationToken cancellationToken);
 }
